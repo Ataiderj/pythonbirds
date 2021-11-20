656fff1dfa73447e3d673d2aa52d1ef7a1099471
Ataiderj <55640402+Ataiderj@users.noreply.github.com>
2021-11-20 19:25:37 -0300

    class Pessoal:       pass

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,11 +2,14 @@ class Pessoa:
     def __init__(self, nome = None, idade = 35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f" Olá {id(self)}"
 
 if __name__ == '__main__':
+    ataide = Pessoa(nome = "Ataíde")
+    luciano = Pessoa(ataide, nome = "Luciano")
     p = Pessoa("luciano")
     print(Pessoa.cumprimentar(p))
     print(id(p))
